a58638a43510f51d7de19116fd1ed01d4c4b837c
yaneeck <tomasz@yaneeck.pl>
2023-12-02 21:36:29 +0100

    day08: v1

---
+++ days/07-09-data-structures/cars.py
@@ -0,0 +1,53 @@
+from typing import Dict, List
+
+
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+DEFAULT_SEARCH = "trail"
+CarsType = Dict[str, List[str]]
+
+
+def get_all_jeeps(cars: CarsType = cars) -> str:
+    """
+    Retrieve the 'Jeep' models from the cars dict and join them by a
+    comma and space (', '). Leave the original ordering intact.
+    """
+    return ', '.join(cars['Jeep'])
+
+
+def get_first_model_each_manufacturer(cars: CarsType = cars) -> List[str]:
+    """
+    Loop through the cars dict filtering out the first model for each
+    manufacturer. Return the matching models in a list leaving the original
+    ordering intact.
+    """
+    return [company[0] for company in cars.values()]
+
+
+def get_all_matching_models(
+    cars: CarsType = cars, grep: str = DEFAULT_SEARCH
+) -> List[str]:
+    """
+    Return a list of all models containing the case insensitive
+    'grep' string which defaults to DEFAULT_SEARCH ('trail').
+    Sort the resulting sequence alphabetically
+    """
+    found_cars: List[str] = []
+    for company in cars.values():
+        for car in company:
+            if grep in car.lower():
+                found_cars.append(car)
+    return found_cars
+
+
+def sort_car_models(cars: CarsType = cars) -> CarsType:
+    """
+    Loop through the cars dict returning a new dict with the
+    same keys and the values sorted alphabetically.
+    """
+    pass
